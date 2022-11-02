7115beda98b5bd2932b7692395a8a6149d56f45d
batchen <b0504144053@gmail.com>
2022-11-02 16:22:24 +0200

    ex0

--- targil0/targil0/Program.cs
+++ targil0/targil0/Program.cs
@@ -4,4 +4,3 @@ Console.WriteLine("hello batchen3");//dvorah wrote it
 Console.WriteLine("Hello DvorahH");//batchen wrote it
 Console.WriteLine("you see it?");//dvorah wrote it
 Console.WriteLine("how are you?");//batchen wrote it
-
