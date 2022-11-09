72c94dc675bb2db00c4db13c812cfde2ec05ce05
batchen <b0504144053@gmail.com>
2022-11-09 15:15:57 +0200

    batchen-side-1

---
+++ Stage0/Program5788.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+namespace Stage0
+{
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+            welcome5788();
+
+            Console.ReadKey();
+        }
+
+        private static void welcome5788()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
+
+
--- targil0/targil0/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("hello batchen3");//dvorah wrote it
-Console.WriteLine("Hello DvorahH");//batchen wrote it
-Console.WriteLine("you see it?");//dvorah wrote it
-Console.WriteLine("how are you?");//batchen wrote it
