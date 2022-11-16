4c8ff04acba661a4dc5d8ff324bad9e04000a088
batchen <b0504144053@gmail.com>
2022-11-16 12:17:32 +0200

    16/11

---
+++ DalFacade/DalApi/ICrud .cs
@@ -0,0 +1,19 @@
+﻿using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    public interface ICrud<T>
+    {
+        //Add,Delete,Update,Get
+        public int Add(T value);
+        public void Delete(int id);
+        public void Update(T p);
+        public T Get(int id);
+        public IEnumerable<T> GetAll();
+    }
+}
---
+++ DalFacade/DalApi/IOrderItem.cs
@@ -0,0 +1,15 @@
+﻿using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    public interface IOrderItem: ICrud<OrderItem>
+    {
+        public OrderItem readByOrderAndProduct(int idOrder, int idProduct);
+        public OrderItem[] readByOrder(int idOrder);
+    }
+}
