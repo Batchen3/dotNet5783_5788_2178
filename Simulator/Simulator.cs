f368a5f7f0355b3b541d9db8d30620a2e263b9ac
Dvorah <vhassoun26@gmail.com>
2023-02-16 16:52:58 +0200

    start-7

---
+++ Simulator/Simulator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    internal static  class Simulator
+    {
+        private static BO.EStatus _prev;
+        private static BO.EStatus _next;
+       // private static event EventHandler stopEvent;
+        public static void run()
+        {
+            Thread thread; 
+        }
+        public static void stop()
+        {
+
+        }
+    }
+}
